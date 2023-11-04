7f7439d6d6324e2810ae51276dc03cd3caf36cea
jwan <510129061@qq.com>
2023-11-04 18:45:38 +1100

    test

--- tools.py
+++ tools.py
@@ -3,7 +3,6 @@ def average(list):
     transposed = zip(* list)
     averages = [sum(column) / len(column) for column in transposed]
     return averages
-
 def choose_device():
     if torch.cuda.is_available():
         device = torch.device("cuda")
